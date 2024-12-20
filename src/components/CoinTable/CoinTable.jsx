823a1744c95ccfba6dc57d95446dd5272b85ecc6
Dhruv <dhruvgautam380@gmail.com>
2024-12-20 19:00:46 +0530

    Added react Query config

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,6 +1,51 @@
 import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../Services/FetchCoinData";
+import { useQueries, useQuery } from "react-query";
+import { use } from "react";
 
 function CoinTable() {
-  return <div>CoinTable</div>;
+  //UseEffect
+  // useEffect(() => {
+
+  //   fetchCoinData(1, "usd"); //function
+  // }, []);
+
+  //alternative to useEffect
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error } = useQuery(
+    ["coins", page],
+    () => {
+      fetchCoinData(page, "usd");
+    },
+    {
+      retry: 2,
+      retryDelay: 1000,
+      cacheTime: 1000 * 60 * 2,
+    }
+  );
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error:{error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  //return
+  return (
+    <div>
+      CoinTable
+      <button onClick={() => setPage(page + 1)}>click Me</button>
+      {page}
+    </div>
+  );
 }
 export default CoinTable;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  //  apply react query in whole application
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
-)
+  </QueryClientProvider>
+);
