12152cbdb964bdc16749675dec490a68dce13a2f
Dhruv <dhruvgautam380@gmail.com>
2024-12-21 12:18:16 +0530

    Added coin table UI with Catche

--- src/Services/fetchCoinData.js
+++ src/Services/fetchCoinData.js
@@ -4,8 +4,8 @@ const perPage = 10;
 
   try{
     const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
-    console.log(response);
-    return response;
+    // console.log(response);
+    return response.data;
   }catch(error){
     console.error(error);
     return null;
--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -14,37 +14,93 @@ function CoinTable() {
   const [page, setPage] = useState(1);
   const { data, isLoading, isError, error } = useQuery(
     ["coins", page],
-    () => {
-      fetchCoinData(page, "usd");
-    },
+    () => fetchCoinData(page, "usd"),
     {
-      retry: 2,
-      retryDelay: 1000,
+      // retry: 2,
+      // retryDelay: 1000,
       cacheTime: 1000 * 60 * 2,
+      staleTime: 1000 * 60 * 2,
     }
   );
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
   if (isError) {
     return <div>Error:{error.message}</div>;
   }
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   //return
   return (
-    <div>
-      CoinTable
-      <button onClick={() => setPage(page + 1)}>click Me</button>
-      {page}
+    // <div>
+    //   CoinTable
+    //   <button onClick={() => setPage(page + 1)}>click Me</button>
+    //   {page}
+    // </div>
+    <div className="flex  flex-col justify-center mx-auto my-5 align-center gap-5 w[80vw] ">
+      <div className="flex justify-center w-[80vw] mx-auto px-2 py-4 font-semibold text-black bg-yellow-400 item-center">
+        {/* Header of table*/}
+        <div className="basis-[35%]">Coin</div>
+        <div className="basis-[25%]">Price</div>
+        <div className="basis-[20%]">24 Change</div>
+        <div className="basis-[20%]">Market Cap</div>
+      </div>
+      <div className="flex flex-col w-[80vw] mx-auto">
+        {isLoading && <div>Loading...</div>}
+        {data &&
+          data.map((coin) => {
+            return (
+              <div
+                key={coin.id}
+                className="flex items-center justify-between w-full px-2 py-4 font-semibold text-white bg-transparent"
+              >
+                <div className="flex item-center justify-start gap-3 basis[35%]">
+                  <div className="w-[5rem] h-[5rem]">
+                    <img
+                      src={coin.image}
+                      alt="coin Image"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div className="flex flex-col basis-[35%]">
+                    <div className="text-3xl">{coin.name}</div>
+                    <div className="text-xl">{coin.symbol}</div>
+                  </div>
+                </div>
+                <div className="flex basis-[25%]">
+                  <div>{coin.current_price}</div>
+                </div>
+                <div className="flex basis-[20%]">
+                  <div>{coin.price_change_24h}</div>
+                </div>
+                <div className="flex basis-[20%]">
+                  <div>{coin.market_cap}</div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+      <div className="flex justify-center gap-4 item-center ">
+        <button
+          disabled={page == 1}
+          onClick={() => setPage(page + 1)}
+          className="text-2xl text-white btn btn-primary btn-wide"
+        >
+          prev
+        </button>
+        <button
+          onClick={() => setPage(page + 1)}
+          className="text-2xl text-white btn btn-secondary btn-wide"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
