835015dffe1177d7848a20f32a54f07ff66eac8b
Dhruv <dhruvgautam380@gmail.com>
2024-12-23 11:53:49 +0530

    Added code spliting with lazy loading and Suspense:

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -1,15 +1,32 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../../pages/Home";
-import CoinDetailsPage from "../../pages/CoinDetailsPage";
+import { List } from "react-content-loader";
 import MainLayout from "../../pages/Layout";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("../../pages/Home")); //Lazy loading
+const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage")); //Lazy loading
 
 function Routing() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<List />}>
+              <Home />
+            </Suspense>
+          }
+        />
         {/**Rendered in outlet component of main layout */}
-        <Route path="/details/:coinId" element={<CoinDetailsPage />} />{" "}
+        <Route
+          path="/details/:coinId"
+          element={
+            <Suspense fallback={<div>Loading Coin Details...</div>}>
+              <CoinDetailsPage />
+            </Suspense>
+          }
+        />{" "}
       </Route>
     </Routes>
   );
